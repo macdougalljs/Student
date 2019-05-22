cc3261f77899987ff5086302b64e76e803411fbe
macdougalljs <50675767+macdougalljs@users.noreply.github.com>
2019-05-22 14:08:16 -0400

    Update StudentTester.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * list to print each name.
 
  * @author Sivagama
- May 22nd 2019 */
+ Wed May 22nd 2019 */
 public class StudentTester {
     public static void main(String[] args)
     {
